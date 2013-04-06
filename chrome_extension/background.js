ec0a6abb0020e4f654eea7a654a0a2569965ab79
Petr Vydra <petr.vydra@gmail.com>
2013-04-06 17:05:56 +0200

    Initial import of Chrome Extension

---
+++ chrome_extension/background.js
@@ -0,0 +1,19 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Called when the url of a tab changes.
+function checkForValidUrl(tabId, changeInfo, tab) {
+  
+  
+  
+  if (tab.url.indexOf('g') > -1) {
+    // ... show the page action.
+    chrome.pageAction.setPopup({tabId: tabId, popup: "popup.html"});
+    chrome.pageAction.show(tabId);
+    
+  }
+};
+
+// Listen for any changes to the URL of any tab.
+chrome.tabs.onUpdated.addListener(checkForValidUrl);
