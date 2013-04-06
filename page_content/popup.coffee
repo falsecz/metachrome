72720f4ff1b73d50515aa0d1cf550f1bd94e8e5f
lukas.weber <lukas.weber@socialbakers.com>
2013-04-07 00:28:19 +0200

    dalsi lesteni

--- page_content/popup.coffee
+++ page_content/popup.coffee
@@ -1,4 +1,4 @@
 chrome.runtime.sendMessage type:'url', (response) ->
-	h = document.querySelector 'h1'
-	h.innerText = response.url
+	#h = document.querySelector 'h1'
+	#h.innerText = response.url
 
--- page_content/popup.js
+++ page_content/popup.js
@@ -2,8 +2,4 @@
 
 chrome.runtime.sendMessage({
   type: 'url'
-}, function(response) {
-  var h;
-  h = document.querySelector('h1');
-  return h.innerText = response.url;
-});
+}, function(response) {});
